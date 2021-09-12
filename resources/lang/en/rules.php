a0ae0d6e9a3d846f1c88907e2d558f21c57df040
GitzJoey <gitzjoey@live.com>
2021-09-12 12:23:42 +0800

    api token paradigm fixed

--- resources/lang/en/rules.php
+++ resources/lang/en/rules.php
@@ -4,4 +4,5 @@ return [
     'same_email' => 'Email cannot be changed.',
     'unchanged_role_name' => 'Role name cannot be changed.',
     'valid_dropdown' => 'Invalid Value',
+    'too_many_tokens' => 'Too many token request',
 ];
--- resources/lang/id/rules.php
+++ resources/lang/id/rules.php
@@ -3,5 +3,6 @@
 return [
     'same_email' => 'Email tidak dapat dirubah.',
     'unchanged_role_name' => 'Nama peran tidak dapat dirubah.',
-    'valid_dropdown' => 'Nilai tidak valid.'
+    'valid_dropdown' => 'Nilai tidak valid.',
+    'too_many_tokens' => 'Terlalu banyak permintaan token',
 ];
